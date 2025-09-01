ed302c0f33f7fb082b579bd24c0edd7c83ee62d2
Angela Lee <angelalee@Angelas-MacBook-Air.local>
2025-08-31 19:46:35 -0500

    Module 1 Exercises

--- src/module1/_1whyjava/E02PoemExample.java
+++ src/module1/_1whyjava/E02PoemExample.java
@@ -5,5 +5,7 @@ public class E02PoemExample {
 	public static void main(String[] args) {
 		System.out.println("Roses are red,");
 		System.out.println("Violets are blue,");
+		System.out.println("It's the first week of school");
+		System.out.println("Let's lock in dude");
 	}
 }
